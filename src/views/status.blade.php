60ccaa13899e320d53899f9d38415780b249f7a3
King Paulo Aquino <me@kpa.ph>
2024-04-18 21:48:09 +0800

    Updated Assets URL

--- src/views/status.blade.php
+++ src/views/status.blade.php
@@ -3,8 +3,8 @@
 
 <head>
     <title>MSG4wrd.io</title>
-    <link rel="apple-touch-icon" href="https://msg4wrd.io/images/kpa-logo-128px-black.png">
-    <link rel="shortcut icon" type="image/png" href="https://msg4wrd.io/images/kpa-logo-128px-black.png" />
+    <link rel="apple-touch-icon" href="https://doorway.msg4wrd.io/images/kpa-logo-128px-black.png">
+    <link rel="shortcut icon" type="image/png" href="https://doorway.msg4wrd.io/images/kpa-logo-128px-black.png" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <style>
         body {
